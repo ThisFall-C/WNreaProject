cf521ca39d39518287518be5795d7a3e1fb2df9b
ThisFall-C <1254414437@qq.com>
2020-05-27 08:58:19 +0800

    5.27五次

--- WNrea/WNrea/WNrea_Start/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WNrea_Start.MvcApplication" Language="C#" %>
---
+++ 毕业项目源码和数据库/WNrea/WNrea/WNrea_Start/Controllers/PowerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WNrea_Start.Controllers
+{
+    public class PowerController : Controller
+    {
+        // GET: Power
+        public ActionResult PowerPage()
+        {
+            return View();
+        }
+    }
+}
